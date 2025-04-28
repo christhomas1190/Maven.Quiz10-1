812143b74fa5434214e852f67f4a1f10a0fcf9ca
christhomas1190 <111439861+christhomas1190@users.noreply.github.com>
2025-04-28 15:08:15 -0400

    didnt fork it so its been i had to delete my old folder and make a new one.

--- src/main/java/rocks/zipcode/quiz4/collections/WordCounter.java
+++ src/main/java/rocks/zipcode/quiz4/collections/WordCounter.java
@@ -1,12 +1,35 @@
 package rocks.zipcode.quiz4.collections;
 
+
+import java.util.HashMap;
 import java.util.Map;
 
+
 public class WordCounter {
+    public String[] strings;
+
+
     public WordCounter(String... strings) {
+        this.strings = strings;
     }
 
+
     public Map<String, Integer> getWordCountMap() {
-        return null;
+        Map<String, Integer> wordCountMap = new HashMap<>();
+
+
+        for (String str : strings) {
+            String[] words = str.split(" ");
+
+
+            for (String word : words) {
+                if (!word.isEmpty()) {
+                    wordCountMap.put(word, wordCountMap.getOrDefault(word, 0) + 1);
+                }
+            }
+        }
+
+
+        return wordCountMap;
     }
 }
--- src/main/java/rocks/zipcode/quiz4/collections/culonary/Food.java
+++ src/main/java/rocks/zipcode/quiz4/collections/culonary/Food.java
@@ -1,5 +1,6 @@
 package rocks.zipcode.quiz4.collections.culonary;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -7,14 +8,23 @@ import java.util.Map;
  * @author leon on 27/12/2018.
  */
 public class Food {
+    private List<Spice> spices = new ArrayList<>();
+
+
     public List<Spice> getAllSpices() {
-        return null;
+
+
+        return spices;
     }
+
 
     public <SpiceType extends Class<? extends Spice>> Map<SpiceType, Integer> getSpiceCount() {
         return null;
     }
 
+
     public void applySpice(Spice spice) {
+        spices.add(spice);
     }
 }
+
